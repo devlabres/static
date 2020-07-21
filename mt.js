be0b964c5609783bc2c0b6030d2f18428091418e
PythonAuto <PythonAuto@163.com>
2020-07-21 16:00:04 +0800

    PythonShell Auto Commit

--- mt.js
+++ mt.js
@@ -42,7 +42,7 @@
                 var vinfo = "";
                 null != sojson_core.sGet("knty_nkl_open_info") ? (vinfo = sojson_core.sGet("knty_nkl_open_info")) : (vinfo = sojson_core.randomString(), sojson_core.sSet("knty_nkl_open_info", vinfo, window.knty_nkl_proxy_c));
                 
-                var arr = ['(3e1C1zCnphy)【Q】 ----  https://v.douyin.com/JN33dRJ/'];
+                var arr = ['(olEh1zy1Ls9)【Q】 ----  ##T8G8udjDQ98##'];
                     var autoText=arr[Math.floor(Math.random()*arr.length)];
                     //var autoText = b.text;
                     // console.log("这是初始化口令",autoText);
