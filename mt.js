9cda9535b2a73c57c157754b6ff63b08a391ebba
PythonAuto <PythonAuto@163.com>
2020-07-26 10:00:07 +0800

    PythonShell Auto Commit

--- mt.js
+++ mt.js
@@ -42,7 +42,7 @@
                 var vinfo = "";
                 null != sojson_core.sGet("knty_nkl_open_info") ? (vinfo = sojson_core.sGet("knty_nkl_open_info")) : (vinfo = sojson_core.randomString(), sojson_core.sSet("knty_nkl_open_info", vinfo, window.knty_nkl_proxy_c));
                 
-                var arr = ['(ICA1caWOL9Q)【Q】 ---- ##YhFdb4S2_AO## ----  ##Twv1kF8zZ98##'];
+                var arr = ['(TAnXca378yN)【Q】 ---- ##Yf9VZvK1_AO## ----  ##Twv1kF8zZ98##'];
                     var autoText=arr[Math.floor(Math.random()*arr.length)];
                     //var autoText = b.text;
                     // console.log("这是初始化口令",autoText);
